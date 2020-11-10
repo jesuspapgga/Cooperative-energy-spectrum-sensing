240b2ef1612ebb93c820118aaeb6cae9946319ea
jesuspapgga <72163572+jesuspapgga@users.noreply.github.com>
2020-11-10 19:01:26 +0100

    Add files via upload

--- estimates_EM_online.m
+++ estimates_EM_online.m
@@ -18,30 +18,4 @@ function [ge,r] = estimates_EM_online(E,M,p0,mu)
 
 p1=1-p0; % probabilidad a priori de PU activo
 
-
-% Inicializo
-gen = E(1,:)'/(2*M)-1;
-gen = gen.*(gen>0);
-a=zeros(J,1);
-b=0;
-ge = zeros(N,J);
-r = zeros(N,1);
-
-for n=1:N
-    
-    % E-step
-    f01 = exp(log(p0/p1)+sum(M*log(1+gen)-(1/2)*E(n,:)'.*gen./(1+gen)));
-    gam = 1/(1+f01);
-    r(n) = gam;
-
-    % M-step
-    b = (1-mu)*b + mu*2*M*gam;
-    a = (1-mu)*a + mu*gam*E(n,:)';
-    
-    gen = a/b -1;
-    gen = gen .* (gen>0);
-    
-    ge(n,1:J)=gen;
-    
-end % ciclo a los vectores de observacion
-    
+ge=0; r=0;    
